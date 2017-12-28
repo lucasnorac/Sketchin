675eaeac84b325f980bbfa0f3310a5b1e2ddc8cf
lucasnorac <lucas_albarello_92@hotmail.com>
2017-12-28 13:01:58 -0200

    Initial Commit

---
+++ Sketchin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sketchin
+//
+//  Created by Lucas Caron Albarello on 28/12/2017.
+//  Copyright © 2017 Lucas Caron Albarello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
